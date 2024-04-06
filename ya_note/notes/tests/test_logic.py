3d4761f2874e4302cf367bfbc41457aeeb8803f8
marisgan <marisgan@gmail.com>
2024-04-06 14:11:46 +0300

    correct PEP8 warning

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -53,7 +53,7 @@ class TestLogic(BaseSetup):
             self.author_client.post(ADD_URL, data=data_slug_dublicate),
             form='form',
             field='slug',
-            errors=f'{data_slug_dublicate.get('slug')}{WARNING}'
+            errors=f'{self.note.slug}{WARNING}'
         )
         self.assertEqual(list(Note.objects.all()), notes_before)
 
